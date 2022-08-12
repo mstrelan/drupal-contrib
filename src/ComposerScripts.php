8fc68398eb21cd618c15f23197f373837b9c1abc
Michael Strelan <mstrelan@gmail.com>
2022-08-12 16:18:18 +1000

    Configure PhpStorm via post-root-package-install script

--- src/ComposerScripts.php
+++ src/ComposerScripts.php
@@ -16,8 +16,26 @@ class ComposerScripts {
       return;
     }
     $io = $event->getIO();
-    $io->write("Cloning Drupal core into 'app'.");
+    $io->write("Cloning Drupal core into 'app'");
     system('git clone https://git.drupalcode.org/project/drupal.git app');
+  }
+
+  /**
+   * Configures initial PhpStorm settings.
+   */
+  public static function configurePhpStorm(Event $event): void {
+    if (file_exists('.idea')) {
+      return;
+    }
+    $io = $event->getIO();
+    if ($io->isInteractive() && !$io->askConfirmation('<info>Do you want to automatically configure PhpStorm?</info> [<comment>Y,n</comment>]? ')) {
+      return;
+    }
+    $io->write("Configuring PhpStorm");
+    mkdir('.idea');
+    copy('assets/scaffold/files/php.xml', '.idea/php.xml');
+    copy('assets/scaffold/files/symfony2.xml', '.idea/symfony2.xml');
+    copy('assets/scaffold/files/workspace.xml', '.idea/workspace.xml');
   }
 
 }
