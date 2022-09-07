c3c2a275510fedfc6a13dfe6c56878a08002d64a
Michael Strelan <mstrelan@gmail.com>
2022-09-08 09:51:30 +1000

    Try with `id -u` and `id -g` instead

--- src/ComposerScripts.php
+++ src/ComposerScripts.php
@@ -22,8 +22,8 @@ class ComposerScripts {
       'UID=1000',
       'GID=1000',
     ], [
-      sprintf('UID=%d', posix_geteuid()),
-      sprintf('GID=%d', posix_getegid()),
+      sprintf('UID=%d', system('id -u')),
+      sprintf('GID=%d', system('id -g')),
     ], $dist));
     $io->write(file_get_contents('.env'));
   }
