9cb785afef28ee79d704a6a122ba981748e86c47
Michael Strelan <mstrelan@gmail.com>
2022-09-08 10:07:39 +1000

    Revert "Try with `id -u` and `id -g` instead"
    This reverts commit c3c2a275510fedfc6a13dfe6c56878a08002d64a.

--- src/ComposerScripts.php
+++ src/ComposerScripts.php
@@ -22,8 +22,8 @@ class ComposerScripts {
       'UID=1000',
       'GID=1000',
     ], [
-      sprintf('UID=%d', system('id -u')),
-      sprintf('GID=%d', system('id -g')),
+      sprintf('UID=%d', posix_geteuid()),
+      sprintf('GID=%d', posix_getegid()),
     ], $dist));
     $io->write(file_get_contents('.env'));
   }
